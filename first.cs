d489136b616929bd5ba62ce25366e9f2f44a83a5
lilianbriney <134567610+lilianbriney@users.noreply.github.com>
2023-11-27 18:21:23 -0500

    Update first.cs

--- first.cs
+++ first.cs
@@ -37,3 +37,91 @@ class Program
     Console.Write("Enter date of purchase: ");
     string purchaseDate = Console.ReadLine();
     Console.Write("Enter expiration date: ");
+     Console.Write("Enter the quantity: ");
+    int quantity = int.Parse(Console.ReadLine());
+
+    InventoryItem item = new InventoryItem
+    {
+      Name = name,
+      PurchaseDate = purchaseDate,
+      ExpirationDate = expirationDate,
+      Quantity =  quantity
+    };
+
+    inventory.Add(item);
+    Console.WriteLine("Item added sucessfully!");
+    
+  }
+    static void RemoveItem()
+  {
+    if (inventory.Count == 0)
+    {
+      Console.WriteLine("Inventory is empty. Cannot remove items.");
+      return;
+      }
+//fix this area
+    Console.WriteLine("Current Inventory: ");
+    for (int i = 0
+    {
+      Console.WriteLine($"{i + 1"}. {inventory[i].Name}");
+    }
+      Console.Write("Enter the number of the item to remove: ");
+    int choice = int.Parse(Console.ReadLine());
+
+    if (choice >= 1 && choice <= inventory.Count)
+  {
+    InventoryItem removedItem = inventory[choice - 1];
+    inventory.RemoveAt(choice - 1);
+    Console.WriteLine($"{removeItem.Name} remove from inventory.");
+  }
+  else
+  {
+      Console.WriteLine("Invalid choice. Please enter a valid item number.");
+  }
+  static void ViewInventory()
+{
+    if (inventory.Count == 0)
+    {
+        Console.WriteLine("Inventory is empty.");
+        return;
+     }
+
+    Console.WriteLine("\nInventory:");
+    for (int i = 0; i < inventory.Count; i++)
+      {
+          Console.WriteLine($"{i + 1}. Name {inventory[i].Name}, Purchase Date: {inventory[i].PurchaseDate}, Expiration Date: {inventory[i].ExpirationDate}, Quantity: {inventory[i].Quantity}");
+       }
+}
+    static void Main()
+
+{
+  while (true)
+    {
+    //want to change this method
+    DisplayMenu();
+    ConsoleKeyInfo key = Console.ReadKey();
+
+    switch (char.ToUpper(key.KeyChar());
+      {
+          case 'A':
+            AddItem();
+            break;
+          case'R':
+            RemoveItem();
+            break;
+            case 'V':
+          ViewInventory();
+            break;
+            case 'X':
+            Console.WriteLine("\nExiting the menu. Goodbye!");
+            return;
+            default:
+              Console.WriteLine("n\Invalid choice. Please enter a number between 1 and 4.");
+              
+  
+    
+      }
+    }
+  }
+}
+
